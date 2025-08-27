61538bb375f62365ebfc17892ac6a3f39008092e
Hades <rizkyputrafajar55@gmail.com>
2025-08-27 11:23:50 +0700

    Update Migrations

--- database/migrations/2025_08_25_104321_create_portofolios_table.php
+++ database/migrations/2025_08_25_104321_create_portofolios_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image', 255);
-            $table->string('description',255);
+            $table->string('description',999);
             $table->timestamps();
         });
     }
