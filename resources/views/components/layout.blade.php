1b756955c771103861903a1eb4ce2dd5394d6fff
Hades <rizkyputrafajar55@gmail.com>
2025-09-01 13:45:53 +0700

    Penambahan Favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,14 @@
   <title>{{ config('app.name', 'Olympus Project') }}</title>
 
 
+  <!-- Favicon -->
+  <link rel="icon" href="{{ asset('favicon/favicon.ico') }}" sizes="any"> <!-- fallback -->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+  <meta name="theme-color" content="#ffffff">
+
 
   {{-- Fonts (opsional) --}}
   <link rel="preconnect" href="https://fonts.bunny.net">
